b5a793578b6aa90ec617e6c40d3c52e2d254f58a
lariat2525 <uran252525@gmail.com>
2024-04-08 19:36:30 +0900

    ADD:TEST Emotion

---
+++ app/(pages)/template/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { css } from "@emotion/react";
+
+const tempStyle = css`
+  color: blue;
+  font-size: 16px;
+`;
+
+export default function Template() {
+  return <main css={tempStyle}>test</main>;
+}
