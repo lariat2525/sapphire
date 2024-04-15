1a761b3b272c3660751b83ab4ef1b661a2319253
lariat2525 <uran252525@gmail.com>
2024-04-15 14:37:15 +0900

    ADD:Tailwind remove emotion

--- app/(pages)/template/page.tsx
+++ app/(pages)/template/page.tsx
@@ -1,16 +1,9 @@
 "use client";
-import { css } from "@emotion/react";
 import { useRecoilState } from "recoil";
 import { templateState } from "@/state/template";
 
 /* TSX */
 export default function Template() {
   const [template, setTemplate] = useRecoilState(templateState);
-  return <main css={tempStyle}>{template}</main>;
+  return <main>{template}</main>;
 }
-
-/* CSS */
-const tempStyle = css`
-  color: blue;
-  font-size: 16px;
-`;
