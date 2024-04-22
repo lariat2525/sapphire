243d2dbb17839cd0f0f1c36cf96f5ffa8887d8a9
lariat2525 <uran252525@gmail.com>
2024-04-22 17:00:38 +0900

    ADD:TailWind test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 "use client";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { RecoilRoot } from "recoil";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -17,7 +14,7 @@ export default function RootLayout({
         <meta name="description" content="Description" />
       </head>
       <RecoilRoot>
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </RecoilRoot>
     </html>
   );
