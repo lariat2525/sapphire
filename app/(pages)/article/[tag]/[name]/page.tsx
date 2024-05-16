f7633b3fa083d8b88af25179a18410a8d93f8bed
Arinosu0814 <sapingpu@gmail.com>
2024-05-16 21:15:25 +0900

    ADD: 記事ページのstyle調整　
    DotLineの作成

--- app/(pages)/article/[tag]/[name]/page.tsx
+++ app/(pages)/article/[tag]/[name]/page.tsx
@@ -10,55 +10,48 @@ export default function Article() {
       <div className="Left mx-4 w-2/3">
         <div className="Info">
           <div className="Image bg-secondary-color">
-            <img src="" alt="image" className="utl-article-image-size" />
+            <img src="/test/goblin.svg" alt="image" className="h-" />
           </div>
           {/* TODO: 後で"h-screen"消してね👍 */}
-          <div className="Detail bg-white h-screen">
-            <div className="Profile">
-              <div className="Labels">
-                <div className="Label">
-                  <h3 className="p-2 mt-0.5 text-2xl">プロフィール</h3>
-                </div>
-                <div className="Line flex">
-                  <DotLine></DotLine>
-                </div>
-              </div>
+          <div className="Detail p-4 bg-white h-screen">
+            <div className="Profile m-4">
               <div className="Names p-5">
                 <div className="Name flex justify-center">
                   <div className="EN text-5xl">
                     <p>GOBLIN</p>
                   </div>
-                  <div className="JP flex items-end">
+                  <div className="JP text-2xl ml-2 flex items-end">
                     <p className="inline-block">ゴブリン</p>
                   </div>
                 </div>
-                <div className="Tags flex justify-end">
-                  <div className="Tag flex px-2">
-                    <div>Dot</div>
-                    <div>Fantasy</div>
+                <div className="Tags text-1xl flex justify-end">
+                  <div className="Tag flex bg-tertiary-color text-white px-1 utl-flex-center border-2 rounded-full w-1/4">
+                    <div className="h-2 w-2 mr-1.5 rounded-full bg-white"></div>
+                    <div className="mx-3">Fantasy</div>
                   </div>
-                  <div className="Tag flex px-2">
-                    <div>Dot</div>
-                    <div>God</div>
+                  <div className="Tag flex bg-tertiary-color text-white px-1 utl-flex-center border-2 rounded-full w-1/4">
+                    <div className="h-2 w-2 mr-1.5 rounded-full bg-white"></div>
+                    <div className="ml-3 mr-4">God</div>
                   </div>
                 </div>
               </div>
+              <DotLine></DotLine>
               <div className="Text">
                 <p className="px-8 my-0.5 leading-9 text-base tracking-widest">
                   アンスプラッシュとは、大量の写真が保管されているストックフォトの一つです。そのため、わざわざアンスプラッシュからダウンロードして取り込まなくても、Figmaアプリなら直接かつ簡単に写真をUIデザインに追加できるので、時間短縮になり著しく業務が改善されます。また、写真の挿入にはアンスプラッシュだけでなくペクセルというプラグインも利用可能です。
                 </p>
               </div>
             </div>
-            <div className="Root">
+            <div className="Root m-4">
               <div className="Labels">
                 <div className="Label">
-                  <h3 className="p-2 my-0.5 text-2xl">由来</h3>
+                  <h3 className="p-2 mt-0.5 text-2xl">由来</h3>
                 </div>
                 <div className="Line flex">
                   <DotLine></DotLine>
                 </div>
               </div>
-              <div className="Text">
+              <div className="Text mt-6">
                 <p className="inline-block px-8 my-0.5 leading-9 text-base tracking-widest">
                   写真の背景を取り除くためだけに、写真編集ソフトに時間を費やしていませんか？そういう時は、FigmaアプリにRemoveBGのプラグインをインストールすれば解決できます。わざわざ他のツールを使わなくても、RemoveBGでデザインファイル内にある写真の背景を簡単に取り除けます。
                 </p>
@@ -68,7 +61,7 @@ export default function Article() {
         </div>
       </div>
       <div className="Right mx-4 w-1/3">
-        <div className="Graphs bg-tertiary-color text-white utl-size-h-600 mb-6 border-2 rounded">
+        <div className="Graphs bg-tertiary-color text-white utl-size-h-550 mb-6 border-2 rounded">
           <div className="Graph h-80">
             <div className="Label flex justify-center">
               <h2 className="inline-block m-2 utl-flex-center border-2 rounded-full w-1/5">
@@ -84,12 +77,12 @@ export default function Article() {
               </h2>
             </div>
             <div className="Data">
-              <div className="Size my-2 mx-5 p-2 flex border-b">
+              <div className="Size my-2 mx-5 flex border-b">
                 <div className="Name mx-2 px-2">
-                  <p className="inline-block">大きさ</p>
+                  <p className="">大きさ</p>
                 </div>
                 <div className="Value">
-                  <p className="ml-24 px-2">約130㎝</p>
+                  <p className="ml-10 px-2">約130㎝</p>
                 </div>
               </div>
               <div className="Weight my-2 mx-5 p-2 flex border-b">
@@ -97,7 +90,7 @@ export default function Article() {
                   <p className="">体重</p>
                 </div>
                 <div className="value">
-                  <p className="utl-ml-104 px-2">約60kg</p>
+                  <p className="ml-10 px-2">約60kg</p>
                 </div>
               </div>
               <div className="Habitat my-2 mx-5 flex">
@@ -105,7 +98,7 @@ export default function Article() {
                   <p className="">生息地</p>
                 </div>
                 <div className="Value">
-                  <p className="ml-24 px-2">オランダ</p>
+                  <p className="ml-9 px-2">オランダ</p>
                 </div>
               </div>
             </div>
@@ -115,7 +108,7 @@ export default function Article() {
         {/* TODO: 後で"h-screen"消してね👍 */}
         <div className="Category bg-secondary-color mt-6 h-screen">
           <div className="Labels">
-            <div className="Label p-2 mt-0.5 text-2xl">
+            <div className="Label p-2 ml-5 mt-0.5 text-2xl">
               <p>カテゴリー</p>
             </div>
             <div className="LineDotCenter flex">
--- components/elements/shapes/DotLine/index.tsx
+++ components/elements/shapes/DotLine/index.tsx
@@ -3,7 +3,10 @@ import styles from "./style.module.scss";
 const DotLine = () => {
   return (
     <div className={styles.temp}>
-      <h1>Hello World</h1>
+      <div className="h-6 ml-5 utl-flex-center">
+        <div className="h-3 w-3 m rounded-full bg-black" />
+        <div className="ml-3 mr-4 my-4 utl-size-w-500 flex border-b-4 border-black" />
+      </div>
     </div>
   );
 };
