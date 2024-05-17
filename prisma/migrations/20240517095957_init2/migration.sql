4d03cebe27e19d39007cac56b845c2e70c308f05
Arinosu0814 <sapingpu@gmail.com>
2024-05-17 19:18:17 +0900

    ADD: Database再調整

--- prisma/migrations/20240517095957_init2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `preview` to the `Articles` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Articles" ADD COLUMN     "preview" INTEGER NOT NULL;
