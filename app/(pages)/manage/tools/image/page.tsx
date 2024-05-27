a0aeb9c2744c54ca1d80337a5971d0361c56fac2
lariat2525 <uran252525@gmail.com>
2024-05-27 14:52:55 +0900

    ADD:manage削除/デプロイ時のみ

--- app/(pages)/manage/tools/image/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useRecoilState } from "recoil";
-import { templateState } from "@/state/template";
-
-/* TSX */
-export default function Template() {
-  const [template, setTemplate] = useRecoilState(templateState);
-  return <main>{template}</main>;
-}
